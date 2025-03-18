e5e2d25d4840f3a35f55febd38e41656d67da2c5
Abhishek <abhishekchaudhari1021@gmail.com>
2025-03-18 11:22:10 +0530

    erefe

--- Graph.cpp
+++ Graph.cpp
@@ -1,121 +1,179 @@
-/*
-Assignment 3: - There are flight paths between cities. If there is a flight between city A and city B then there is an edge between the cities.
-                The cost of the edge can be the time that flight takes to reach city B from A or the amount of fuel used for the journey.
-                The node can be represented by airport name or name of the city. Use adjacency list representation of the graph and adjacency
-                matrix representation of the graph. Justify the storage representation used
-
-
-                create a city name array
-*/
-
 #include <iostream>
-// #include <string>
+#include <string>
 
 using namespace std;
 
 class Graph
 {
-    string city[50], source, dest;
-    int fuel, v, i, j, edges;
+    string city[50];
     int AdjMatrix[50][50];
-    Graph *temp, *next, *newNode;
+    struct Node
+    {
+        int index;
+        int fuel;
+        Node *next;
+    };
+
+    Node *head[50];
+    int v, edges;
 
 public:
+    Graph()
+    {
+        v = 0;
+        edges = 0;
+        for (int i = 0; i < 50; i++)
+        {
+            head[i] = nullptr;
+            for (int j = 0; j < 50; j++)
+            {
+                AdjMatrix[i][j] = 0;
+            }
+        }
+    }
+
     void acceptByAdjMatrix();
     void acceptByAdjList();
-    void display();
-    // void adjacencyList();
-    // void adjacencyMatrix();
-} *head[50];
+    void displayMatrix();
+    void displayList();
+};
 
-// void Graph::acceptByAdjMatrix()
-// {
-//     cout << "Enter the number of cities: ";
-//     cin >> v;
-
-//     cout << "Enter the city names: " << endl;
-//     for (int i = 0; i < v; i++)
-//     {
-//         cout << "City " << i + 1 << " : ";
-//         cin >> city[i];
-//     }
-//     cout << "Enter the fuel for the cities : ";
-//     for (int i = 0; i < v; i++)
-//     {
-//         for (int j = 0; j < v; j++)
-//         {
-//             if (i == j)
-//             {
-//                 AdjMatrix[i][j] = 0;
-//             }
-//             else
-//             {
-//                 cout << "\nEnter the fuel for " << city[i] << " to " << city[j] << " : ";
-//                 cin >> AdjMatrix[i][j];
-//             }
-//         }
-//     }
-// }
-
-void Graph::acceptByAdjList()
+void Graph::acceptByAdjMatrix()
 {
     cout << "Enter the number of cities: ";
     cin >> v;
+
     cout << "Enter the city names: " << endl;
     for (int i = 0; i < v; i++)
     {
         cout << "City " << i + 1 << " : ";
         cin >> city[i];
     }
-    cout << "Enter the number of edges: ";
+
+    cout << "Enter the fuel costs between cities (-1 for no direct path):" << endl;
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            if (i == j)
+            {
+                AdjMatrix[i][j] = 0; // No self-loop
+            }
+            else
+            {
+                cout << "Fuel cost from " << city[i] << " to " << city[j] << ": ";
+                cin >> AdjMatrix[i][j];
+                if (AdjMatrix[i][j] < 0) // No direct path
+                {
+                    AdjMatrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+void Graph::acceptByAdjList()
+{
+    cout << "Enter the number of cities: ";
+    cin >> v;
+
+    cout << "Enter the city names: " << endl;
+    for (int i = 0; i < v; i++)
+    {
+        cout << "City " << i + 1 << " : ";
+        cin >> city[i];
+    }
+
+    cout << "Enter the number of direct flights (edges): ";
     cin >> edges;
 
     for (int i = 0; i < edges; i++)
     {
-        cout << "Enter the source and destination cities : ";
-        cin >> source >> dest;
+        string source, dest;
+        int fuelCost;
+        cout << "Enter source city: ";
+        cin >> source;
+        cout << "Enter destination city: ";
+        cin >> dest;
+        cout << "Enter fuel required: ";
+        cin >> fuelCost;
+
+        int srcIndex = -1, destIndex = -1;
+
+        // Find the indices of source and destination
         for (int j = 0; j < v; j++)
         {
-            if (source == city[j])
-            {
-                break;
-            }
+            if (city[j] == source)
+                srcIndex = j;
+            if (city[j] == dest)
+                destIndex = j;
         }
-    }
 
-    newNode = new Graph();
-    newNode->city = dest;
-    cout << "Enter the fuel required : ";
-    cin >> newNode->fuel;
-    newNode->next = NULL;
-
-    temp = head[j];
-    if (temp == NULL)
-    {
-        head[j] = newNode;
-    }
-    else
-    {
-        while (temp->next != NULL)
+        if (srcIndex == -1 || destIndex == -1)
         {
-            temp = temp->next;
+            cout << "Invalid city names. Try again.\n";
+            i--;
+            continue;
         }
-        temp->next = newNode;
+
+        // Create new node
+        Node *newNode = new Node();
+        newNode->index = destIndex;
+        newNode->fuel = fuelCost;
+        newNode->next = nullptr;
+
+        // Insert into adjacency list
+        if (head[srcIndex] == nullptr)
+        {
+            head[srcIndex] = newNode;
+        }
+        else
+        {
+            Node *temp = head[srcIndex];
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
     }
 }
 
-void Graph::display()
+void Graph::displayMatrix()
 {
-    cout << "\nCities with fuel required : " << endl;
+    cout << "\nAdjacency Matrix Representation:\n";
+    cout << "   ";
     for (int i = 0; i < v; i++)
     {
+        cout << city[i] << " ";
+    }
+    cout << "\n";
+
+    for (int i = 0; i < v; i++)
+    {
+        cout << city[i] << " ";
         for (int j = 0; j < v; j++)
         {
             cout << AdjMatrix[i][j] << " ";
         }
         cout << "\n";
     }
-    cout << "\n";
+}
+
+void Graph::displayList()
+{
+    cout << "\nAdjacency List Representation:\n";
+    for (int i = 0; i < v; i++)
+    {
+        cout << city[i] << " -> ";
+        Node *temp = head[i];
+        while (temp != nullptr)
+        {
+            cout << city[temp->index] << " (Fuel: " << temp->fuel << ") -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
 }
 
 int main()
@@ -124,10 +182,12 @@ int main()
     int choice;
     do
     {
+        cout << "\nMenu:\n";
         cout << "1. Accept by adjacency matrix\n";
         cout << "2. Accept by adjacency list\n";
-        cout << "3. Display\n";
-        cout << "4. Exit\n";
+        cout << "3. Display adjacency matrix\n";
+        cout << "4. Display adjacency list\n";
+        cout << "5. Exit\n";
         cout << "Enter choice: ";
         cin >> choice;
         switch (choice)
@@ -139,15 +199,18 @@ int main()
             g.acceptByAdjList();
             break;
         case 3:
-            g.display();
+            g.displayMatrix();
             break;
         case 4:
-            cout << "Exiting...";
+            g.displayList();
+            break;
+        case 5:
+            cout << "Exiting...\n";
             break;
         default:
-            cout << "Invalid choice. Try again.";
+            cout << "Invalid choice. Try again.\n";
             break;
         }
-    } while (choice != 4);
+    } while (choice != 5);
     return 0;
 }
--- MST.cpp
+++ MST.cpp
@@ -5,19 +5,107 @@ time use appropriate and algortihm and data structure to solve the problem and i
 */
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-class MST
+class MST // Minimum Spanning Tree
 {
-    string city[50], source, dest;
-    int fuel, v, i, j, edges;
-    int AdjMatrix[50][50];
-    MST *temp, *next, *newNode;
+    string city[10];
+    int min, v, e, u;
+    int adjMatrix[10][10];
+    bool visited[10];
+    int key[10], parent[10];
 
 public:
-    void accept();
-    // void acceptByAdjList();
-    void display();
+    void create();
+    void edges();
+    void displayByPrimsAlgorithm();
 };
+
+void MST::create()
+{
+    cout << "Enter the number of cities: ";
+    cin >> v;
+    cout << "Enter the city names: " << endl;
+    for (int i = 0; i < v; i++)
+    {
+        cout << "City " << i + 1 << " : ";
+        cin >> city[i];
+    }
+}
+
+void MST::edges(){
+    cout << "Enter the number of edges: ";
+    cin >> e;
+    for (int i = 0; i < v; i++){
+        for (int j = 0; j < v; j++){
+            if (i == j){
+                adjMatrix[i][j] = 0;
+            }
+            else{
+                cout << "\nEnter the fuel for " << city[i] << " to " << city[j] << " : ";
+                cin >> adjMatrix[i][j];
+                // adjMatrix[j][i] = adjMatrix[i][j];
+            }
+        }
+    }
+}
+
+void MST::displayByPrimsAlgorithm(){
+    for (int i = 0; i < v; i++){
+        visited[i] = false;
+        min = 999;
+        key[0] = 0;
+        parent[0] = -1;
+    }
+    for (int edge = 0; edge < v - 1; edge++){
+        min = 999;
+        for (int i = 0; i < v; i++){
+            if (!visited[i] && key[i] < min){
+                min = key[i];
+                u = i;
+            }
+
+            visited[u] = true;
+
+            for (int j = 0; j < v; j++){
+                if (adjMatrix[u][j] && !visited[j] && adjMatrix[u][j] < key[j]){
+                    parent[j] = u;
+                    key[j] = adjMatrix[u][j];
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    int choice;
+    MST m;
+    do
+    {
+        cout << "\n1. Accept\n2. Add Edges\n3. Display\n4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            m.create();
+            break;
+        case 2:
+            m.edges();
+            break;
+        case 3:
+            m.displayByPrimsAlgorithm();
+            break;
+        case 4:
+            cout << "Exiting..." << endl;
+            break;
+        default:
+            cout << "Invalid choice" << endl;
+            break;
+        }
+    } while (choice != 4);
+
+    return 0;
+}
